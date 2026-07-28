14fc616e0c2a39aeb1bc16eafa3210dce379674d
letov7482-ui <letov7482@gmail.com>
2026-07-28 20:15:45 +0300

    Update HudElement.java

--- src/main/java/aether/hud/HudElement.java
+++ src/main/java/aether/hud/HudElement.java
@@ -1,23 +1,23 @@
 package aether.hud;
 
+
 import net.minecraft.client.gui.DrawContext;
+
 
 public abstract class HudElement {
 
+
     private final String name;
 
-    protected float x;
-    protected float y;
-
-    protected float width;
-    protected float height;
 
     private boolean enabled = true;
 
-    private boolean dragging;
 
-    private float dragOffsetX;
-    private float dragOffsetY;
+
+    protected float x;
+
+    protected float y;
+
 
 
     public HudElement(
@@ -29,50 +29,60 @@ public abstract class HudElement {
         this.name = name;
 
         this.x = x;
+
         this.y = y;
+
     }
 
 
-    public final void render(DrawContext context) {
 
-        if (!enabled) {
-            return;
-        }
+    public abstract void render(
+            DrawContext context,
+            float tickDelta
+    );
 
-        draw(context);
-    }
-
-
-    protected abstract void draw(DrawContext context);
-
-
-    public void tick() {
-    }
 
 
     public String getName() {
+
         return name;
+
     }
+
+
+
+    public boolean isEnabled() {
+
+        return enabled;
+
+    }
+
+
+
+    public void setEnabled(
+            boolean enabled
+    ) {
+
+        this.enabled = enabled;
+
+    }
+
 
 
     public float getX() {
+
         return x;
+
     }
+
 
 
     public float getY() {
+
         return y;
+
     }
 
-
-    public float getWidth() {
-        return width;
-    }
-
-
-    public float getHeight() {
-        return height;
-    }
 
 
     public void setPosition(
@@ -81,73 +91,9 @@ public abstract class HudElement {
     ) {
 
         this.x = x;
+
         this.y = y;
-    }
 
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-
-    public void setEnabled(
-            boolean enabled
-    ) {
-
-        this.enabled = enabled;
-    }
-
-
-    public boolean isHovering(
-            double mouseX,
-            double mouseY
-    ) {
-
-        return mouseX >= x &&
-                mouseX <= x + width &&
-                mouseY >= y &&
-                mouseY <= y + height;
-    }
-
-
-    public void mousePressed(
-            double mouseX,
-            double mouseY
-    ) {
-
-        if (isHovering(mouseX, mouseY)) {
-
-            dragging = true;
-
-            dragOffsetX =
-                    (float) mouseX - x;
-
-            dragOffsetY =
-                    (float) mouseY - y;
-        }
-    }
-
-
-    public void mouseDragged(
-            double mouseX,
-            double mouseY
-    ) {
-
-        if (!dragging) {
-            return;
-        }
-
-        x =
-                (float) mouseX - dragOffsetX;
-
-        y =
-                (float) mouseY - dragOffsetY;
-    }
-
-
-    public void mouseReleased() {
-
-        dragging = false;
     }
 
 }
