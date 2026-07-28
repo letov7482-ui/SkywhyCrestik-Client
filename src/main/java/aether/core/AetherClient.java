95c0fc1f02a5adf2b6051c6214647fa4b48e766f
letov7482-ui <letov7482@gmail.com>
2026-07-28 17:31:57 +0300

    Create AetherClient.java

---
+++ src/main/java/aether/core/AetherClient.java
@@ -0,0 +1,38 @@
+package aether.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class AetherClient {
+
+    private static final AetherClient INSTANCE = new AetherClient();
+
+    public static final String NAME = "Aether Visuals";
+    public static final String VERSION = "1.0.0";
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(NAME);
+
+    private AetherClient() {
+    }
+
+    public static AetherClient getInstance() {
+        return INSTANCE;
+    }
+
+    public void initialize() {
+
+        LOGGER.info("==========================================");
+        LOGGER.info("{} is starting...", NAME);
+        LOGGER.info("Version: {}", VERSION);
+
+        Managers.initialize();
+
+        LOGGER.info("{} successfully loaded!", NAME);
+        LOGGER.info("==========================================");
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+}
