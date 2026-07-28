1998d446110265af9ecfbc19d4bf0d2fdedc7d2c
letov7482-ui <letov7482@gmail.com>
2026-07-28 17:33:01 +0300

    Create Managers.java

---
+++ src/main/java/aether/core/Managers.java
@@ -0,0 +1,45 @@
+package aether.core;
+
+import aether.config.ConfigManager;
+import aether.event.EventBus;
+import aether.gui.notification.NotificationManager;
+import aether.hud.HudManager;
+import aether.module.ModuleManager;
+import aether.particle.ParticleManager;
+import aether.theme.ThemeManager;
+
+public final class Managers {
+
+    public static ModuleManager MODULE;
+    public static HudManager HUD;
+    public static ThemeManager THEME;
+    public static ConfigManager CONFIG;
+    public static NotificationManager NOTIFICATION;
+    public static ParticleManager PARTICLE;
+    public static EventBus EVENT_BUS;
+
+    private Managers() {
+    }
+
+    public static void initialize() {
+
+        EVENT_BUS = new EventBus();
+
+        THEME = new ThemeManager();
+
+        MODULE = new ModuleManager();
+
+        HUD = new HudManager();
+
+        NOTIFICATION = new NotificationManager();
+
+        PARTICLE = new ParticleManager();
+
+        CONFIG = new ConfigManager();
+
+        CONFIG.load();
+
+        AetherClient.getLogger().info("Managers initialized successfully.");
+    }
+
+}
