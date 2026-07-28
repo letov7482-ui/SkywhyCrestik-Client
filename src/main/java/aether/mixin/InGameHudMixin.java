fecdd734d95191635bf90a43341858bae2291715
letov7482-ui <letov7482@gmail.com>
2026-07-28 18:36:48 +0300

    Create InGameHudMixin.java

---
+++ src/main/java/aether/mixin/InGameHudMixin.java
@@ -0,0 +1,36 @@
+package aether.mixin;
+
+import aether.core.Managers;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+
+    @Inject(
+            method = "render",
+            at = @At("TAIL")
+    )
+    private void renderHud(
+            DrawContext context,
+            float tickDelta,
+            CallbackInfo ci
+    ) {
+
+        if (Managers.HUD == null) {
+            return;
+        }
+
+
+        Managers.HUD.render(
+                context
+        );
+
+    }
+
+}
