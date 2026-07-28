9cb80809fd04a65dec71c4dfcdc456e8fb15d2a1
letov7482-ui <letov7482@gmail.com>
2026-07-28 18:29:31 +0300

    Update Managers.java

--- src/main/java/aether/core/Managers.java
+++ src/main/java/aether/core/Managers.java
@@ -6,7 +6,6 @@ import aether.gui.notification.NotificationManager;
 import aether.hud.HudManager;
 import aether.module.ModuleManager;
 import aether.particle.ParticleManager;
-import aether.theme.ThemeManager;
 
 public final class Managers {
 
@@ -22,29 +21,25 @@ public final class Managers {
 
     public static void initialize() {
 
-        AetherClient.getLogger().info("Initializing Event Bus...");
+        AetherClient.getLogger()
+                .info("Initializing managers...");
+
         EVENT_BUS = new EventBus();
 
-        AetherClient.getLogger().info("Initializing Theme Engine...");
-        ThemeManager.initialize();
-
-        AetherClient.getLogger().info("Initializing Module Manager...");
         MODULE = new ModuleManager();
 
-        AetherClient.getLogger().info("Initializing HUD Manager...");
         HUD = new HudManager();
 
-        AetherClient.getLogger().info("Initializing Notification Manager...");
         NOTIFICATION = new NotificationManager();
 
-        AetherClient.getLogger().info("Initializing Particle Manager...");
         PARTICLE = new ParticleManager();
 
-        AetherClient.getLogger().info("Initializing Config Manager...");
         CONFIG = new ConfigManager();
+
         CONFIG.load();
 
-        AetherClient.getLogger().info("All managers initialized successfully.");
+        AetherClient.getLogger()
+                .info("Managers initialized.");
 
     }
 
