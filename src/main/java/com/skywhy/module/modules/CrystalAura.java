aada2490a34e58413fe3ed837a1183a5b5245b7d
letov7482-ui <letov7482@gmail.com>
2026-07-28 15:48:57 +0300

    Create CrystalAura.java

---
+++ src/main/java/com/skywhy/module/modules/CrystalAura.java
@@ -0,0 +1,6 @@
+package com.skywhy.module.modules;
+import com.skywhy.module.Module;
+
+public class CrystalAura extends Module {
+    public CrystalAura() { super("CrystalAura", Category.COMBAT); }
+}
