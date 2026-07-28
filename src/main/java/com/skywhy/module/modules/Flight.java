b33f22102c81a81da49f4c06990a5766619d315a
letov7482-ui <letov7482@gmail.com>
2026-07-28 15:23:02 +0300

    Update Flight.java

--- src/main/java/com/skywhy/module/modules/Flight.java
+++ src/main/java/com/skywhy/module/modules/Flight.java
@@ -1,6 +1,7 @@
 package com.skywhy.module.modules;
 
 import com.skywhy.module.Module;
+import net.minecraft.util.math.Vec3d;
 
 public class Flight extends Module {
     public Flight() { super("Flight", Category.MOVEMENT); }
@@ -9,7 +10,7 @@ public class Flight extends Module {
         if (mc.player == null) return;
         mc.player.getAbilities().flying = true;
         mc.player.getAbilities().setFlySpeed(0.05f);
-        mc.player.getVelocity().y = 0;
+        mc.player.setVelocity(new Vec3d(mc.player.getVelocity().x, 0, mc.player.getVelocity().z));
     }
     @Override
     public void onDisable() {
