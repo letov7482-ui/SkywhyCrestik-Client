e811034793a79e6d1c0c24d4082c0dc08325460b
letov7482-ui <letov7482@gmail.com>
2026-07-28 20:29:36 +0300

    Update Managers.java

--- src/main/java/aether/core/Managers.java
+++ src/main/java/aether/core/Managers.java
@@ -1,45 +1,97 @@
 package aether.core;
 
+
 import aether.config.ConfigManager;
 import aether.event.EventBus;
 import aether.gui.notification.NotificationManager;
 import aether.hud.HudManager;
+import aether.hud.HudRenderer;
 import aether.module.ModuleManager;
 import aether.particle.ParticleManager;
+import aether.theme.ThemeManager;
+
+
 
 public final class Managers {
 
+
     public static ModuleManager MODULE;
+
     public static HudManager HUD;
+
+    public static ThemeManager THEME;
+
     public static ConfigManager CONFIG;
+
     public static NotificationManager NOTIFICATION;
+
     public static ParticleManager PARTICLE;
+
     public static EventBus EVENT_BUS;
 
+
+
     private Managers() {
+
     }
+
+
+
+
 
     public static void initialize() {
 
-        AetherClient.getLogger()
-                .info("Initializing managers...");
 
-        EVENT_BUS = new EventBus();
 
-        MODULE = new ModuleManager();
+        EVENT_BUS =
+                new EventBus();
 
-        HUD = new HudManager();
 
-        NOTIFICATION = new NotificationManager();
 
-        PARTICLE = new ParticleManager();
+        THEME =
+                new ThemeManager();
 
-        CONFIG = new ConfigManager();
+
+
+        MODULE =
+                new ModuleManager();
+
+
+
+        HUD =
+                new HudManager();
+
+
+
+        NOTIFICATION =
+                new NotificationManager();
+
+
+
+        PARTICLE =
+                new ParticleManager();
+
+
+
+        CONFIG =
+                new ConfigManager();
+
+
+
+        EVENT_BUS.register(
+                new HudRenderer()
+        );
+
+
 
         CONFIG.load();
 
+
+
         AetherClient.getLogger()
-                .info("Managers initialized.");
+                .info(
+                        "Managers initialized successfully."
+                );
 
     }
 
